9c2ff863bd142e0fb214ac2563ad2949a16f9d2b
liuzhangren100 <759551480@qq.com>
2020-05-18 15:42:38 +0800

    upadte

--- src/components/dataSource.js
+++ src/components/dataSource.js
@@ -1,7 +1,7 @@
 export default [
   {id: 1 ,x: 100,y: 800, divW: 50, divH: 50, imgW: 50, imgH: 50, color: '' }, 
   {id: 2 ,x: 100,y: 701, divW: 46, divH: 50, imgW: 50, imgH: 50, color: '' }, 
-  {id: 3 ,x: 100,y: 603, divW: 46, divH: 50, imgW: 50, imgH: 50, color: ''  }, 
+  {id: 3 ,x: 100,y: 602, divW: 46, divH: 50, imgW: 50, imgH: 50, color: ''  }, 
 
   // {id: 4 ,x: 4, y: 480, divW: 46, divH: 50, imgW: 50, imgH: 50, color: ''  }, 
   // {id: 5 ,x: 4, y: 350, divW: 23, divH: 140, imgW: 27, imgH: 140, color: ''  },
